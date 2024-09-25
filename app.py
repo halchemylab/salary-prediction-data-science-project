20ce592c238de98f5cf2cc7fcd3f0b0a599d43b3
halchemy <harthur96@gmail.com>
2024-09-25 15:29:24 -0700

    - Added streamlit app for Interface

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+st.title("Salary Prediction App")
+
+st.divider() ##############################################################################
+
+st.write("You can get estimations for the salaries of the company employees")
+
+years = st.number_input("Enter the years at company",value = 1, step = 1, min_value = 0)
+jobrate = st.number_input("Enter the job rate", value= 3.5, step = 0.1, min_value = 0.0)
+
+X = [years, jobrate]
+
+model = joblib.load("linearmodel.pkl")
+
+st.divider() ##############################################################################
+
+predict = st.button("Press the button for salary prediction")
+
+st.divider() ##############################################################################
+
+if predict:
+    st.balloons()
+    
+    X1 = np.array([X])
+    prediction = model.predict(X1)
+    st.write(f"Salary Prediction is {prediction}")
+    
+else:
+    "Please press the button for app to make the prediction"
